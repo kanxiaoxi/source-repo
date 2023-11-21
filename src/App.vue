6f184661fc364c56ac5acf1ea472fb5916a7364a
kanxiaoxi <wzxjachinwu@gmail.com>
2023-11-21 20:06:51 +0800

    Styles change font-size

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
+  <div class="app">
     GitHub Actions Practices
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.app {
+  font-size: 28px;
 }
 </style>
